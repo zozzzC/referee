2ae522447ed44019dbd2bbc4567c277029ee3170
zozzzC <czlc05edu@gmail.com>
2024-03-19 15:39:52 +1300

    added primary components--navbar and heading

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>zozchuu</h1>
+            <div className="navlinks">
+                <a href="/home">Home</a>
+                <a href="/portfolio">Portfolio</a>
+                <a href="/prices">Pricelist</a>
+                <a href="/TOS">Terms</a>
+                <a href="/Contact">Contact</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
