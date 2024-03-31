60c2f73392f9f41f78f55e4ec6687630b39d3442
zozzzC <czlc05edu@gmail.com>
2024-03-31 23:06:18 +1300

    fixed image scaling for carousel images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,9 +5,22 @@ import img from "../images/meteor-shower.png";
 
 const Carousel = () => {
   const displayImg = [
-    { key: 1, route: `images/meteor-shower.png`, desc: "meteor shower art" },
-    { key: 2, route: `images/romw.png`, desc: "romw art" },
-    { key: 3, route: `images/kaveh-sad.png`, desc: "kaveh art" },
+    {
+      key: 1,
+      route: require(`../images/meteor-shower.png`),
+      desc: "meteor shower art",
+    },
+
+    {
+      key: 2,
+      route: require(`../images/romw.png`),
+      desc: "romw art",
+    },
+    {
+      key: 3,
+      route: require(`../images/kaveh-sad.png`),
+      desc: "kaveh art",
+    },
   ];
 
   //make sure that images are in the public folder, all assets must be in the public folder for us to properly reference!
