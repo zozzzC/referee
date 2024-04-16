375c5b6fb7709925a37b3eeb7f76bba849c53759
zozzzC <czlc05edu@gmail.com>
2024-04-16 15:15:11 +1200

    added masonry layout and basic masonry resizing

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../index.css";
+import useWindowSize from "../hooks/useWindowResize";
 import Mobile from "../components/Mobile";
 import Footer from "../components/Footer";
 import { useNavigate } from "react-router-dom";
 import PortfolioImg from "../components/PortfolioImages";
+import Masonry from "react-responsive-masonry";
 
 const Portfolio = () => {
   //later, instead of importing everything like this, there may be get request that gets image URLs from a server
@@ -26,21 +28,34 @@ const Portfolio = () => {
       desc: "kaveh art",
     },
     {
+      key: 4,
+      route: require(`../images/furina-bust.png`),
+      desc: "furina art",
+    },
+    {
       key: 3,
       route: require(`../images/man-sketch.png`),
       desc: "old man art",
     },
   ];
 
+  //set the number of columns based on the window size.
+  const isMobile = useWindowSize();
+  let columnsCount = 4;
+
+  if (isMobile == true) {
+    columnsCount = 2;
+  }
+
   return (
     <div className="portfolio">
       <Mobile />
       <h1>portfolio</h1>
-      <div className="images">
+      <Masonry columnsCount={columnsCount} gutter="10px">
         {displayImg.map((img) => (
           <PortfolioImg id={img.id} route={img.route} desc={img.desc} />
         ))}
-      </div>
+      </Masonry>
       <Footer />
     </div>
   );
